b0ef84e74472100e99bf267ade454a668aaf30d6
Sean Healy <sean.healy33@mail.dcu.ie>
2016-02-01 21:38:05 +0000

    Updated data.cljs

--- src/redbrick_20/views/map.cljs
+++ src/redbrick_20/views/map.cljs
@@ -6,7 +6,7 @@
 
 (defn home-did-mount [this]
   (let [map-canvas (reagent/dom-node this)
-        map-options (clj->js {"center" (google.maps.LatLng. 53.3366916, -6.265777)
+        map-options (clj->js {"center" (google.maps.LatLng.   53.3366916, -6.265777)
                               "zoom" 15})]
         (js/google.maps.Map. map-canvas map-options)))
 
